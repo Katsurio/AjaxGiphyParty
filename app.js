80e8aa4dce743122703892815f55735db6eab1fc
John (Jef) Fisher <Fishe545@gmail.com>
2021-07-30 13:06:07 +0800

    feat: added styles for title

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-console.log("Let's get this party started!");
+function randomRGB() {
+  const r = Math.floor(Math.random() * 0)
+  const g = Math.floor(Math.random() * 256)
+  const b = Math.floor(Math.random() * 256)
+  return `rgb(${r},${g},${b})`
+}
+
+const letters = document.querySelectorAll('.letter')
+
+const intervalId = setInterval(function () {
+  for (let letter of letters) {
+    letter.style.color = randomRGB()
+  }
+}, 2000)
